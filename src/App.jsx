646c11d36251bfa9fd8a3aacad72aec0dcc2c395
Arihant Desai <desaiarihant15@gmail.com>
2025-06-13 17:12:12 +0530

    next commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import {  Routes,Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Courses from './pages/Courses';
 import Contact from './pages/Contact';
 import Apply from './pages/Apply';
+import ChatbotComponent from './components/Chatbot/ChatbotComponents';
+import DeveloperInfoPopup from './components/DeveloperInfo/DeveloperInfoPopup';
+
 
 const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
   return (
     <>
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Arihant Mahavir Desai"
+          studentPhotoUrl="/images/Arihant.jpeg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
       <Navbar />
+     
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -18,8 +36,13 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/apply" element={<Apply />} />
       </Routes>
+      
+      <ChatbotComponent/>
+      
     </>
   );
 };
 
 export default App;
+
+
